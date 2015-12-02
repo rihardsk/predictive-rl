be0c11cadb433b723df0ec79db3734f0100467f5
Rihards Krišlauks <rihards.krislauks@gmail.com>
2015-12-02 20:18:59 +0200

    moving all-in-one experiment stuff to a seperate class

---
+++ predictive_rl/rlglueexperiment/sequential_experiment.py
@@ -0,0 +1,76 @@
+from ipyparallel import Client
+import subprocess
+import shlex
+import atexit
+from multiprocessing import Process
+from predictive_rl.predictive_future_agent import PredictiveFutureAgent as pfe
+from predictive_rl.experiment import RLExperiment
+import os
+
+
+class SequentialExperiment(object):
+    def __init__(self):
+        self.subprocesses = set()
+        self.processes = set()
+
+        @atexit.register
+        def kill_subprocesses():
+            for subproc in self.subprocesses:
+                print "killing"
+                print subproc
+                subproc.kill()
+            for proc in self.processes:
+                print "killing"
+                print proc
+                proc.terminate()
+
+    def run_env(self, rlglue_port):
+        env_command = "RLGLUE_PORT={0} java -jar /home/rihards/Programming/Multi/rl/rl-library/rl-library/products/CartPole.jar".format(rlglue_port)
+        self.subprocesses.add(subprocess.Popen(env_command, shell=True))
+
+    def run_rlglue(self, rlglue_port):
+        glue_command = "RLGLUE_PORT={0} rl_glue".format(rlglue_port)
+        self.subprocesses.add(subprocess.Popen(glue_command, shell=True))
+
+    def run_agent(self, rlglue_port, agent_args):
+        os.environ["RLGLUE_PORT"] = str(rlglue_port)
+        agent = pfe(**agent_args)
+        proc = Process(target=agent.run)
+        self.processes.add(proc)
+        proc.start()
+
+    def run_experiment(self, rlglue_port, exp_args):
+        os.environ["RLGLUE_PORT"] = str(rlglue_port)
+        exp = RLExperiment(**exp_args)
+        proc = Process(target=exp.run)
+        self.processes.add(proc)
+        proc.start()
+
+    def run_and_wait(self, rlglue_port, agent_args, exp_args):
+        self.run_env(rlglue_port)
+        self.run_rlglue(rlglue_port)
+        self.run_agent(rlglue_port, agent_args)
+        self.run_experiment(rlglue_port, exp_args)
+
+        for subproc in self.subprocesses:
+            subproc.wait()
+        for proc in self.processes:
+            proc.join()
+
+
+def main():
+    # rc = Client()
+    # dview = rc[:]
+    #
+    # @dview.parallel(block=True)
+    # def run_exp(rlglue_port, env_args, rlglue_args, agent_args, exp_args):
+    #     processes = set()
+    #     processes.add(subprocess.Popen())
+    seqexp = SequentialExperiment()
+    seqexp.run_and_wait(4096, {"action_stdev": 5}, {"dir": "test"})
+
+if __name__ == "__main__":
+    main()
+
+
+
