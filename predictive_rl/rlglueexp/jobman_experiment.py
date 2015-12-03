e98d4850b9742e506d9fc35cdc0e176b9bc5c0d0
Rihards Krišlauks <rihards.krislauks@gmail.com>
2015-12-03 19:23:52 +0200

    renaming rlglueexperiment -> rlglueexp

--- predictive_rl/rlglueexp/jobman_experiment.py
+++ predictive_rl/rlglueexp/jobman_experiment.py
@@ -1,11 +1,11 @@
-import predictive_rl.rlglueexperiment.sequential_experiment
+import predictive_rl.rlglueexp.sequential_experiment
 
 
 def run_and_wait_parallel(state, channel):
     rlglue_port = state.rlglue_port
     agent_args = state.agent_args
     exp_args = state.exp_args
-    seqexp = predictive_rl.rlglueexperiment.sequential_experiment.SequentialExperiment()
+    seqexp = predictive_rl.rlglueexp.sequential_experiment.SequentialExperiment()
     seqexp.run_and_wait(rlglue_port, agent_args, exp_args)
 
 
